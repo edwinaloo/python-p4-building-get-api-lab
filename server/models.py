cf948daa3bb7cb5a989eb6cd040db0ae5bf0701b
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-06-24 22:12:43 +0300

    final pish

--- server/models.py
+++ server/models.py
@@ -1,20 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+    baked_goods = relationship('BakedGood', backref='bakery', lazy='dynamic')
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
+    bakery_id = db.Column(db.Integer, ForeignKey('bakeries.id'))
+
+
+
     
